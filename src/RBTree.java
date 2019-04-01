a171f21c62d3417b0d29c521a6dd63586639e178
Max Medberry <48782060+mmedberry@users.noreply.github.com>
2019-03-31 22:12:08 -0500

    added team member names

--- src/RBTree.java
+++ src/RBTree.java
@@ -1,7 +1,8 @@
 /**
  * Team members:
- * @author John Doe
- * @author Jane Doe
+ * @author Max Medberry
+ * @author Kenneth Lange
+ * @author Daniil Olshanskyi
  * 
  * RBTree class, maintains operations on RBTree.
  */
